0de29e2f27c3dbbab0fa1ca924e1bfb8f5553846
LuisGallegos <albertogallego356@gmail.com>
2018-11-09 12:34:55 -0300

    guardado

---
+++ database/migrations/2018_11_09_151625_create_personas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePersonasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->increments('idPersona');
+            $table->string('nombre completo');
+            $table->date('fechaN');
+            $table->string('sexo');
+            $table->unsignedInteger('ci');
+            $table->unsignedInteger('telefono');
+            $table->string('color de ojos');
+            $table->string('tipoS');
+            $table->unsignedInteger('estatura');
+            $table->unsignedInteger('peso');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
